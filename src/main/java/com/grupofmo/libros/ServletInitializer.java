650f4b8632dad2bc352a97ab0a6e97b66bb023f6
elgabo82 <gabrielmorejon@gmail.com>
2026-03-20 20:56:51 -0500

    Bases del proyecto

---
+++ src/main/java/com/grupofmo/libros/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.grupofmo.libros;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(LibrosApplication.class);
+	}
+
+}
---
+++ src/test/java/com/grupofmo/libros/LibrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupofmo.libros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
